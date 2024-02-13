7214de6b4d1131eec94fb01daafbc53dc0105e7b
Aguiar575 <aguiarus99arthurus@gmail.com>
2024-02-13 00:42:05 -0300

    moving the dev env to another project

--- dev-env/variables.lua
+++
@@ -1,3 +0,0 @@
-vim.env.PACKER_LOCATION = "/root/.config/nvim/lua/arthur/packer.lua<CR>"
-vim.env.LLVM_PATH = "/usr/bin/lldb-dap-18"
-vim.env.DEBUGGY_PY_PATH = "/root/virtualenvs/debugpy/bin/python"
