19bd56c631ed75f1e2e1cd3477ef5fcbad05dc05
Aguiar575 <aguiarus99arthurus@gmail.com>
2025-02-02 00:19:29 -0300

    updating custom file location; updating git ignore, to ignore bkp files

--- emacs/init.el
+++ emacs/init.el
@@ -171,16 +171,5 @@
   :custom
   (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(evil-magit ivy-rich which-key rainbow-delimiters doom-themes doom-modeline hydra counsel general evil-collection evil marginalia swiper command-log-mode all-the-icons)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(load custom-file 'noerror)
